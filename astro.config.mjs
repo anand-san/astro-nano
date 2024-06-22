869b6f12c433360454a9c1962123bf2c1c945257
Anand <17026547+anand-san@users.noreply.github.com>
2024-06-23 00:10:10 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
